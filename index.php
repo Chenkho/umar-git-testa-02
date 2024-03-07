e776bd5b6f33ba950629441be515545a31b6b778
Chenkho <husainumar107@gmail.comcleargit statusgit commitcleargit config --global user.email husainumar107@gmail.com>
2024-03-07 22:09:19 +0700

    nambah file php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba git remote </title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit facere pariatur sunt quibusdam a? Quasi voluptatem inventore veniam temporibus dolorem necessitatibus, labore eligendi illo quas doloremque fugiat a quidem libero.</p>
+    
+</body>
+</html>
